151c75f17635dd5080b43466e09108998fdc1d90
sumail <sumail@xyzzdev.com>
2025-12-16 12:51:55 +0800

    fix: this is a fix test commit
    BREAKING CHANGE: its breaking

--- host-shell/src/types/global.d.ts
+++ host-shell/src/types/global.d.ts
@@ -2,13 +2,14 @@
  * @Author: sumail sumail@xyzzdev.com
  * @Date: 2025-12-04 12:29:19
  * @LastEditors: sumail sumail@xyzzdev.com
- * @LastEditTime: 2025-12-04 12:29:23
+ * @LastEditTime: 2025-12-16 12:50:02
  * @FilePath: /micro-platform/host-shell/src/types/global.d.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 declare global {
   interface Window {
     _MICRO_APP_ENV: boolean;
+    _TEST: boolean;
   }
 }
 
