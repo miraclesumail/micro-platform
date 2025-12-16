6685305deaffa039f06dec3b5666e05926a7278e
sumail <sumail@xyzzdev.com>
2025-12-16 13:56:14 +0800

    hhh

--- host-shell/src/App.tsx
+++ host-shell/src/App.tsx
@@ -2,11 +2,11 @@
  * @Author: sumail sumail@xyzzdev.com
  * @Date: 2025-12-03 09:16:57
  * @LastEditors: sumail sumail@xyzzdev.com
- * @LastEditTime: 2025-12-03 18:49:28
+ * @LastEditTime: 2025-12-16 13:48:55
  * @FilePath: /micro-platform/host-shell/src/App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { Link, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
 import './App.css';
 
